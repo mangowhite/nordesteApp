a086d6beae8a609d2faab1d89e0278647578a7e6
mangowhite <75037449+mangowhite@users.noreply.github.com>
2022-04-14 16:12:09 +0000

    I removed what wasn't necessary

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ document.querySelector("#select-mudanca").addEventListener(
 );
 
 //GOOGLE DOCS API
-import data from './modelos/local.json' assert {type: 'json'};
-window.addEventListener('load', (e) => {
+window.addEventListener('load', () => {
   authenticate().then(loadClient)
 });
 
@@ -29,42 +28,42 @@ form.addEventListener('submit', (e) => {
 function authenticate() {
   return gapi.auth2.getAuthInstance()
     .signIn({ scope: "https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file" })
-    .then(function () { console.log("Sign-in successful"); },
-      function (err) { console.error("Error signing in", err); });
+    .then(function() { console.log("Sign-in successful"); },
+      function(err) { console.error("Error signing in", err); });
 }
 function loadClient() {
   gapi.client.setApiKey(process.env.APIKEY);
   return gapi.client.load("https://docs.googleapis.com/$discovery/rest?version=v1")
-    .then(function () { console.log("GAPI client loaded for API"); },
-      function (err) { console.error("Error loading GAPI client for API", err); });
+    .then(function() { console.log("GAPI client loaded for API"); },
+      function(err) { console.error("Error loading GAPI client for API", err); });
 }
 // Make sure the client is loaded and sign-in is complete before calling this method.
 function execute(title, body) {
 
   let copy = gapi.client.drive.files.copy({
-    "fileId": "1mer0H2a4yuMWLaRP8vW4x42MLEkI_FTe6xw3HDyxMdU",
+    "fileId": process.env.MODELID,
     "resource": {}
   });
 
-  function renameRequest () {
+  function renameRequest() {
     const init = {
       method: 'PATCH',
       headers: {
         "Authorization": `Bearer  [YOUR_ACCESS_TOKEN]`,
       }
     }
-  } 
+  }
   let id = copy.id
   return gapi.client.docs.documents.batchUpdate({
     "documentId": id,
     "resource": {}
   })
-    .then(function (response) {
+    .then(function(response) {
       // Handle the results here (response.result has the parsed body).
       console.log("Response", response);
     },
-      function (err) { console.error("Execute error", err); });
+      function(err) { console.error("Execute error", err); });
 }
-gapi.load("client:auth2", function () {
-  gapi.auth2.init({ client_id: "process.env.CLIENTID" });
+gapi.load("client:auth2", function() {
+  gapi.auth2.init({ client_id: process.env.APICLIENT });
 });
